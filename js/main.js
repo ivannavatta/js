77a975b89818205add23689b63556ad8585cb16d
ivan navatta <ivannavatta@gmail.com>
2023-07-16 17:30:36 -0300

    agregado d intentos

--- js/main.js
+++ js/main.js
@@ -9,12 +9,13 @@ const Numero_random = Math.ceil(Math.random()*(Max_num - Min_num) + Min_num);
     function jugar(){
     let Numero_jugador = 0;
     let Intentos = 5
+    let intentos = 0
     for(let i = 1; i <= Intentos; i++){
-
+     intentos ++;
     Numero_jugador = parseInt(prompt("Encontra el numero entre el 50 y 100:"));
-
+    
     if(Numero_jugador === Numero_random) {
-        alert("Acertaste el numero era " + Numero_jugador);
+        alert("Acertaste el numero era " + Numero_jugador + ` en ${intentos} intentos`);
         return;
     }
     if(Numero_jugador < Numero_random) {
